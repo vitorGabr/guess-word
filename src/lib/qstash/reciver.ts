3963f17176e14ff77fc13ec2b6c6c3d5b6028889
vitorGabr <macstudios.info@gmail.com>
2024-10-24 18:21:06 -0300

    Remove unused qstash receiver module

--- src/lib/qstash/reciver.ts
+++
@@ -1,6 +0,0 @@
-import { Receiver } from "@upstash/qstash";
-
-export const qstashReciver = new Receiver({
-	currentSigningKey: `${process.env.QSTASH_CURRENT_SIGNING_KEY}`,
-	nextSigningKey: `${process.env.QSTASH_NEXT_SIGNING_KEY}`,
-});
