47a05c1ff581af934ae692a900b2bc54c39d67d7
vitorGabr <macstudios.info@gmail.com>
2024-10-28 17:26:59 -0300

    Refactor GET method to POST in revalidate-page route

--- src/app/api/revalidate-page/route.ts
+++ src/app/api/revalidate-page/route.ts
@@ -2,7 +2,7 @@ import { revalidatePath } from "next/cache";
 import { verifySignatureAppRouter } from "@upstash/qstash/nextjs";
 
 export const dynamic = "force-dynamic";
-export const GET = verifySignatureAppRouter(async () => {
+export const POST = verifySignatureAppRouter(async () => {
 	revalidatePath("/");
 	return Response.json({ success: true });
 });
